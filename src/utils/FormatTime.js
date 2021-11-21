a854e6721ae89b1be3084898da220aec8d18180e
Maciek Mielczarek <maciejka-67@o2.pl>
2021-11-21 10:52:21 +0100

    Add another version of formatTime function (as a comment)

--- src/utils/FormatTime.js
+++ src/utils/FormatTime.js
@@ -4,3 +4,15 @@ export const formatTime = secondsToPromo => (
     :
     null
 );
+
+/*export const formatTime = secondsToPromo => {
+  if(typeof secondsToPromo !== 'number' || secondsToPromo < 0){
+    return null;
+  }
+  const hours = Math.floor(secondsToPromo / 3600);
+  const minutes = Math.floor((secondsToPromo % 3600) / 60);
+  const seconds = Math.floor(secondsToPromo % 60);
+  return [hours, minutes, seconds].map(timePart =>
+    (timePart + '').padStart(2, 0)
+  ).join(':');
+};*/
